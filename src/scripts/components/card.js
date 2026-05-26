18409167f4cc6e61a6a948aa850b5bcff7d51968
Induce <aa5619721@gmail.com>
2026-05-27 01:12:05 +0300

    update card.js

--- src/scripts/components/card.js
+++ src/scripts/components/card.js
@@ -1,5 +1,6 @@
-export const likeCard = (likeButton) => {
+export const likeCard = (likeButton, countElement, cardData) => {
   likeButton.classList.toggle("card__like-button_is-active");
+  countElement.textContent = cardData.likes.length;
 };
 
 export const deleteCard = (cardElement) => {
@@ -15,27 +16,37 @@ const getTemplate = () => {
 
 export const createCardElement = (
   data,
+  currentUserId,
   { onPreviewPicture, onLikeIcon, onDeleteCard }
 ) => {
   const cardElement = getTemplate();
   const likeButton = cardElement.querySelector(".card__like-button");
   const deleteButton = cardElement.querySelector(".card__control-button_type_delete");
   const cardImage = cardElement.querySelector(".card__image");
+  const likeCount = cardElement.querySelector(".card__like-count");
 
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardElement.querySelector(".card__title").textContent = data.name;
+  likeCount.textContent = data.likes.length;
 
-  if (onLikeIcon) {
-    likeButton.addEventListener("click", () => onLikeIcon(likeButton));
+  const isLikedByMe = data.likes.some((user) => user._id === currentUserId);
+  if (isLikedByMe) {
+    likeButton.classList.add("card__like-button_is-active");
   }
 
-  if (onDeleteCard) {
-    deleteButton.addEventListener("click", () => onDeleteCard(cardElement));
+  if (data.owner._id !== currentUserId) {
+    deleteButton.remove();
+  } else if (onDeleteCard) {
+    deleteButton.addEventListener("click", () => onDeleteCard(cardElement, data._id));
+  }
+
+  if (onLikeIcon) {
+    likeButton.addEventListener("click", () => onLikeIcon(likeButton, likeCount, data._id));
   }
 
   if (onPreviewPicture) {
-    cardImage.addEventListener("click", () => onPreviewPicture({name: data.name, link: data.link}));
+    cardImage.addEventListener("click", () => onPreviewPicture({ name: data.name, link: data.link }));
   }
 
   return cardElement;
